4f13ac1996c999fbbe4393c7c5716947c115de3d
sla-ppy <sla-ppy-pm@protonmail.com>
2025-03-09 23:19:38 +0100

    add util.c, cleanup repo

---
+++ src/util.c
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void printTime(FILE* stream) {
+    time_t t = time(NULL); // get local time in calendar format
+
+    char time_buffer[70]; // format local time in calendar to custom format
+
+    if (strftime(time_buffer, sizeof time_buffer, "[%Y.%m.%d - %H:%M:%S] ", localtime(&t))) {
+        fprintf(stream, "%s", time_buffer);
+    } else {
+        fprintf(stderr, "[ERR] getTime() fails to retrietve formatted time\n");
+    }
+}
+
+void checkArgument(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stdout, "Usage: ./server <port>\n");
+        exit(1);
+    }
+    if (argv[1] == NULL) {
+        fprintf(stdout, "Usage: ./server <port>\n");
+        exit(1);
+    }
+    for (int i = 0; isdigit(argv[1][i]) == 0; i++) {
+        fprintf(stdout, "Argument for port may only be a number\n");
+        exit(1);
+    }
+    if (atoi(argv[1]) < 1024 || atoi(argv[1]) > 65535) {
+        fprintf(stdout, "Argument for port out of range. Range: [1024 - 65535]\n");
+        exit(1);
+    }
+}
---
+++ src/util.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#ifndef UTIL_H
+void printTime(FILE* stream);
+void checkArgument(int argc, char** argv);
+#define UTIL_H
+
+#endif // UTIL_H
